b6796c4d9031cb87f1e373dcef24e801a954551d
F-masanori <122769733+f-masanori-9@users.noreply.github.com>
2024-10-09 09:23:34 +0900

    base

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+      compress: true,
+      port: 9001,
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+    mode: isProduction ? "production" : "development",
+  };
+};
